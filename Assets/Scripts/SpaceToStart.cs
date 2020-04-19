a202c00206922526592b9fe8355869ce86068f10
Ville Nuutinen <ville.nuutinen@wunder.dog>
2020-04-19 14:02:23 +0300

    Added start screen

---
+++ Assets/Scripts/SpaceToStart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceToStart : MonoBehaviour
+{
+    public GameObject titleText;
+    public GameObject instructionsText;
+    public GameObject loadingText;
+    
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Start game!");
+            titleText.SetActive(false);
+            instructionsText.SetActive(false);
+            loadingText.SetActive(true);
+
+            StartCoroutine(LoadGameScene());
+        }
+            
+    }
+
+    private IEnumerator LoadGameScene()
+    {
+        var asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
